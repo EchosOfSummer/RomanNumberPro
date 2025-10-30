c0cf045d14f9587441d0aa9747884e105607f2da
Neebin Buswa <neebinnodin.buswa9132@fvtc.edu>
2025-10-29 22:46:13 -0500

    ?

--- RomanNumberPro/App.xaml.cs
+++ RomanNumberPro/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace RomanNumberPro;
+﻿using RomanNumberPro.Views;
+
+namespace RomanNumberPro;
 
 public partial class App : Application
 {
@@ -6,6 +8,51 @@ public partial class App : Application
     {
         InitializeComponent();
 
-        MainPage = new AppShell();
+        var shell = new Shell
+        {
+            Items =
+            {
+                new Tab
+                {
+                    Title = "Number to Roman Numerical Converter",
+                    Icon = "",
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Route = "NumberPage",
+                            ContentTemplate = new DataTemplate(() => new NumberPage())
+                        }
+                    }
+                },
+                new Tab
+                {
+                    Title = "Roman Numerical to Number",
+                    Icon = "",
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Route = "RomanPage",
+                            ContentTemplate = new DataTemplate(() => new RomanPage())
+                        }
+                    }
+                },
+                new Tab
+                {
+                    Title = "About",
+                    Icon = "",
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Route = "AboutPage",
+                            ContentTemplate = new DataTemplate(() => new AboutPage())
+                        }
+                    }
+                }
+            }
+        };
+        MainPage = shell;
     }
 }
