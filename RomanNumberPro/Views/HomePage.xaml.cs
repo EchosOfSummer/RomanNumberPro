9c87a139792f28274b034d4f79a43a5ff9607162
Neebin Buswa <neebinnodin.buswa9132@fvtc.edu>
2025-11-01 21:12:31 -0500

    progress...

---
+++ RomanNumberPro/Views/HomePage.xaml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumberPro.Views;
+
+public partial class HomePage : ContentPage
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+
+    private void Roman(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Number(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void About(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
